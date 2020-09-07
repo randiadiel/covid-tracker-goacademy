60e15ce39099065bfd0d77387cb21b2ce03f68b2
Dennis Chandrian <Dennischandrian@gmail.com>
2020-09-07 12:28:49 +0700

    Initial Commit (Dennis)
    Landing page shadownya kurang blur, 2nd activity masih polos.

--- app/src/main/java/com/example/covidtrackergoacademy/MainActivity.kt
+++ app/src/main/java/com/example/covidtrackergoacademy/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.covidtrackergoacademy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ib_look_up.setOnClickListener() {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("texts", "This is from first activity")
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/covidtrackergoacademy/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.covidtrackergoacademy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val string:String = intent.getStringExtra("texts")
+        textView.text = string
+    }
+}
