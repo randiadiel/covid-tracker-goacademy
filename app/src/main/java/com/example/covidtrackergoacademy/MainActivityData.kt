3667a01529c83270da0849bd0bb02442d3fd1613
Ricco <riccoccir05@gmail.com>
2020-09-12 20:18:46 +0700

    create MainActivityData

---
+++ app/src/main/java/com/example/covidtrackergoacademy/MainActivityData.kt
@@ -0,0 +1,10 @@
+package com.example.covidtrackergoacademy
+
+data class MainActivityData (
+    val name: String,
+    val positiveCases: Int,
+    val recoveredCases: Int,
+    val deathCases: Int,
+    val hospitalizedCases: Int
+
+)
