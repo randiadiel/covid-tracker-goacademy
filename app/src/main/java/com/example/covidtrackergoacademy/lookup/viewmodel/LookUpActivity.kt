7684f83b268b678cccc03401b47e1988379f49cf
Dennis Chandrian <Dennischandrian@gmail.com>
2020-09-14 20:53:59 +0700

    X button on search bar and few bug fixed
    bug fixed:
    - auto-focus on EditText when the activity is started.
    - Can be multiple lines on EditText

--- app/src/main/java/com/example/covidtrackergoacademy/lookup/viewmodel/LookUpActivity.kt
+++ app/src/main/java/com/example/covidtrackergoacademy/lookup/viewmodel/LookUpActivity.kt
@@ -3,9 +3,11 @@ package com.example.covidtrackergoacademy.lookup.viewmodel
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.covidtrackergoacademy.R
 import com.example.covidtrackergoacademy.lookup.LookUpData
@@ -49,11 +51,21 @@ class LookUpActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        ib_cancel.setOnClickListener {
+            et_look_up.setText("")
+        }
+
         et_look_up.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(et_look_up.text.toString() != "") {
+                    ib_cancel.visibility = View.VISIBLE
+                }
+                else {
+                    ib_cancel.visibility = View.INVISIBLE
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
