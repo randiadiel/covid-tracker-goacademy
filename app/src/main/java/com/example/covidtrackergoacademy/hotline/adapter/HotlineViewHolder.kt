d549fe8ba4385842e80870247c2dd53973ae94b5
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-09-17 02:57:03 +0700

    Add Action Dial Intent to hotline recycler view items

--- app/src/main/java/com/example/covidtrackergoacademy/hotline/adapter/HotlineViewHolder.kt
+++ app/src/main/java/com/example/covidtrackergoacademy/hotline/adapter/HotlineViewHolder.kt
@@ -1,7 +1,11 @@
 package com.example.covidtrackergoacademy.hotline.adapter
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.covidtrackergoacademy.R
 import com.example.covidtrackergoacademy.hotline.data.HotlineData
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_hotline.view.*
@@ -10,6 +14,16 @@ class HotlineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(data: HotlineData){
         itemView.tv_item_hotline_name.text = data.name
         itemView.tv_item_hotline_phone.text = data.phone
-        if(data.imgIcon.isNotBlank()) Picasso.get().load(data.imgIcon).into(itemView.iv_item_hotline_image)
+        if(data.imgIcon.isNotBlank()) Picasso.get().load(data.imgIcon).placeholder(R.drawable.progress).into(itemView.iv_item_hotline_image)
+        itemView.btn_item_hotline_call.setOnClickListener{
+            actionDial(data.phone,itemView.context)
+        }
+    }
+    private fun actionDial(phoneNumber : String = "911", context: Context) {
+        val intent = Intent().apply{
+            action = Intent.ACTION_DIAL
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        context.startActivity(intent)
     }
 }
